70f3f0678138da2a7918c5b0c7da87118aeb4200
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-07-15 23:19:24 +0700

    Add files via upload

---
+++ schema.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class CreateUser(BaseModel):
+    name: str
+    surname: str
+    email: str
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserAuth(BaseModel):
+    name: str
+    email: str
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class CreatePost(BaseModel):
+    title: str
+    content: str
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UpdatePost(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
