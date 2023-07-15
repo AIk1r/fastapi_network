171b9d392dcda81c1d8e4ed290da092b337fac6c
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-07-15 23:19:38 +0700

    Add files via upload

---
+++ security.py
@@ -0,0 +1,54 @@
+import jwt
+
+from models import get_user, User, get_db
+from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException, status
+from typing import Optional
+from fastapi_sqlalchemy import db
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+SECRET_KEY = '0bc36ed06853b7aa8dee140d3d680129edb863480c0493a0c2cac26bd5c822a8'
+ALGORITHM = "HS256"
+
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = int(payload.get("sub"))
+        if user_id is None:
+            raise credentials_exception
+    except:
+        raise credentials_exception
+    user = get_user(user_id)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+def create_access_token(user_id: int, expires_delta: Optional[timedelta] = None):
+    to_encode = {"sub": str(user_id)}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
